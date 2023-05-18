474f1ddd0ef85191e7435b7b7ba4df214a7911a1
Ehsan Shahoseini <131699872+3hsan1njast@users.noreply.github.com>
2023-05-18 17:03:45 +0330

    Initial upload

---
+++ main.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val str = "Ayo! Fuck You!"
+    val splitStr = str.split(' ')
+    val result = reverseList(makeListMutable(splitStr)).joinToString(" ")
+    println(result)
+}
+
+fun makeListMutable(list: List<String>): MutableList<String> {
+    val mutableList = mutableListOf<String>()
+    list.forEach { mutableList.add(it) }
+    return mutableList
+}
+
+fun reverseList(list: MutableList<String>): MutableList<String> {
+    val reversedList = mutableListOf<String>()
+    for (i in list.size - 1 downTo 0) {
+        reversedList.add(list[i])
+    }
+    return reversedList
+}
